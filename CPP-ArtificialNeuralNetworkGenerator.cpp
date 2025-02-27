9d62fa237a9d615b507acc7a5b558c3006ae34ee
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-02-27 10:40:50 +1000

    Add project files.

---
+++ CPP-ArtificialNeuralNetworkGenerator.cpp
@@ -0,0 +1,12 @@
+﻿// CPP-ArtificialNeuralNetworkGenerator.cpp : Defines the entry point for the application.
+//
+
+#include "CPP-ArtificialNeuralNetworkGenerator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
