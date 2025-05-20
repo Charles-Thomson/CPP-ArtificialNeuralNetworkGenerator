f09fe192fde48e6fe830b69259955892fe8f52e3
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-05-20 15:11:16 +1000

    Basic sight lines implemented\

--- Environment/EnvironmentObservtions/StateBased2DEnvironmentObservation.cpp
+++ Environment/EnvironmentObservtions/StateBased2DEnvironmentObservation.cpp
@@ -34,24 +34,57 @@ void VectorPrintHelper(vector<StateBasedNode> inputVector) {
 // */
 vector<double> PerformObservationFromLocation(tuple<int, int> observtionPoint, vector<vector<StateBasedNode>> Environment) {
 	
-
-	cout << "Observation location : " << get<0>(observtionPoint) << get<1>(observtionPoint) << endl;
-
 	int observationPointX = get<0>(observtionPoint);
+	int observationPointY = get<1>(observtionPoint);
 
 	int environmentDimension = Environment.size();
 
 
-	vector<tuple<int, int>> sightLineDown = {};
 
-	
 	vector<StateBasedNode> horizontalSlice = Environment[observationPointX];
 	int leftSightLineOffSet = (environmentDimension - observationPointX);
 
+
 	vector<StateBasedNode> leftSightLine(horizontalSlice.begin(), horizontalSlice.end() - leftSightLineOffSet);
+	vector<StateBasedNode> rightSightLine(horizontalSlice.begin() + observationPointX, horizontalSlice.end());
+
+	vector<StateBasedNode> upSightLine;
+	vector<StateBasedNode> downSightLine;
+
+	vector<StateBasedNode> upRightSightLine;
+	vector<StateBasedNode> upLeftSightLine;
+
+	vector<StateBasedNode>	downRightSightLine;
+	vector<StateBasedNode>  downLeftSightLine;
+
+
+	// Collate values "upRight" diagonal from observation point
+	for (int i = observationPointY; i < environmentDimension; ++i) {
+		StateBasedNode value = Environment[i][observationPointY];
+		upRightSightLine.push_back(value);
+	}
+
+
+
+	// Collate values "down" from observation point
+	for (int i = observationPointY; i < environmentDimension; ++i) {
+		StateBasedNode value = Environment[i][observationPointY];
+		downSightLine.push_back(value);
+	}
+	// Collate values "up" from observation point
+	for (int i = observationPointY - 1; i > -1; --i) {
+		StateBasedNode value = Environment[i][observationPointY];
+		upSightLine.push_back(value);
+	}
+
+
+
 
 	VectorPrintHelper(horizontalSlice);
 	VectorPrintHelper(leftSightLine);
+	VectorPrintHelper(rightSightLine);
+	VectorPrintHelper(downSightLine);
+	VectorPrintHelper(upSightLine);
 
 
 	return {};
